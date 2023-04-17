1e1ecd5b6038edcf8a4f751f265c8056f03c527d
www.rubiluqman7@gmail.com <www.rubiluqman7@gmail.com>
2023-04-17 12:48:58 +0300

    commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useContext } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import ThemeContext from "./context/ThemeContext/ThemeContext";
+import FoodProvider from "./context/FoodContext/FoodProvider";
+import Page from "./containers/Page/Page";
+import Recipes from "./containers/Recipes/Recipes";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+
+function App() {
+  const context = useContext(ThemeContext);
+
+  return (
+    <div
+      className={[
+        `theme-${context.theme}`,
+        "container main bg-background-secondary",
+      ].join(" ")}
+    >
+      <Router>
+        <FoodProvider>
+          <Switch>
+            <Route path="/" exact component={Page} />
+            <Route path="/food/:id" component={Recipes} />
+            <Route component={PageNotFound} />
+          </Switch>
+        </FoodProvider>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
